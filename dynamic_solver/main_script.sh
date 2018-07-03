20190e75194f8d60f233af7b12fb3af2caec0608
CedricPerret <cedric.perret.url@gmail.com>
2018-07-03 15:41:50 -0600

    Change nameFile from network structure ER to network structure

--- dynamic_solver/main_script.sh
+++ dynamic_solver/main_script.sh
@@ -4,7 +4,7 @@
 # for ((i=0; i<=100; i=i+10))
 # do
 # 	~/anaconda3/bin/python3 social_consensus_structure.py 1 $i
-# 	~/anaconda3/bin/python3 solver_consensus.py  -n network_structure_ER.json -i influence_distribution.json >> results_ER_250_1000iter.txt
+# 	~/anaconda3/bin/python3 solver_consensus.py  -n network_structure.json -i influence_distribution.json >> results_ER_250_1000iter.txt
 # 	if [[ $(( i % 5 )) == 0 ]]
 # 	then
 # 		echo $i
