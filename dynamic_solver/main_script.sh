52b8de0b1024e58dcdd1ca117a115aeb831a65ba
Andrea Santoro <andrea.santoro@qmul.ac.uk>
2018-07-24 14:28:17 +0100

    Update the bash script adding model name as input

--- dynamic_solver/main_script.sh
+++ dynamic_solver/main_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
- ~/anaconda3/bin/python3 social_consensus_structure.py 0 0
- for ((i=0; i<=100; i=i+10))
- do
- 	~/anaconda3/bin/python3 social_consensus_structure.py 1 $i
- 	~/anaconda3/bin/python3 solver_consensus.py  -n network_structure.json -i influence_distribution.json >> results_ER_250_1000iter.txt
- 	if [[ $(( i % 5 )) == 0 ]]
- 	then
- 		echo $i
- 	fi
- done
-
+name=$1
+echo $name
+~/anaconda3/bin/python3 social_consensus_new.py 0 0 $name
+for ((i=0; i<=100; i=i+5))
+do
+	~/anaconda3/bin/python3 social_consensus_new.py 1 $i $name
+	~/anaconda3/bin/python3 solver_consensus.py  -n network_structure_$name.json -i influence_distribution_$name.json >> results_$name.txt
+	if [[ $(( i % 5 )) == 0 ]]
+	then
+		echo $i
+	fi
+done
