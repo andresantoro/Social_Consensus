4c7fe53cc3c07daca2ac42dc5509c218957f82e8
CedricPerret <cedric.perret.url@gmail.com>
2018-11-05 15:17:46 +0000

    Add files via upload
    Add argument for power law exponent in social consensus structure + loop for replicates

--- dynamic_solver/main_script.sh
+++ dynamic_solver/main_script.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-name=$1
+name=POWER
 echo $name
-~/anaconda3/bin/python3 social_consensus_structure.py 0 0 $name
-for ((i=0; i<=100; i=i+5))
+for ((j=0; j<=8; j=j+4))
 do
-	~/anaconda3/bin/python3 social_consensus_structure.py 1 $i $name
-	~/anaconda3/bin/python3 solver_consensus.py  -n network_structure_$name.json -i influence_distribution_$name.json >> results_quenched_$name.txt
-    ~/anaconda3/bin/python3 solver_consensus.py -mm annealed -n network_structure_$name.json -i influence_distribution_$name.json >> results_annealed_$name.txt
-	if [[ $(( i % 5 )) == 0 ]]
-	then
-		echo $i
-	fi
+	#jdec=$(bc <<<"scale=2; $j/20");
+	/anaconda3/bin/python3 social_consensus_structure.py 0 0 $name $j
+#	for ((i=0; i<=200; i=i+1))
+#	do
+#		#for ((k=0; k<=200; k=k+1))
+#		#do
+#			/anaconda3/bin/python3 social_consensus_structure.py 1 $i $name 0
+#		#	echo -n "$k " >> results_quenched_$name.txt
+#			echo -n "$i " >> results_quenched_$name.txt
+#			echo -n "$j " >> results_quenched_$name.txt
+#			/anaconda3/bin/python3 solver_consensus.py -n network_structure_$name.json -i influence_distribution_$name.json -s 200 >> results_quenched_$name.txt
+
+	    	#/anaconda3/bin/python3 solver_consensus.py -mm annealed -n network_structure_$name.json -i influence_distribution_$name.json >> results_annealed_$name.txt
+#			if [[ $(( i % 1 )) == 0 ]]
+#			then
+#				echo "propL = $i, skew = $j" 
+#			fi
+		#done	
+#	done
 done
